473d26c1ddc8e41de0274484c7e4e9fa896e3342
magamboa34 <134195728+magamboa34@users.noreply.github.com>
2023-05-24 00:28:36 +0800

    Update finals_agamboa_.py

--- finals_agamboa_.py
+++ finals_agamboa_.py
@@ -37,6 +37,6 @@ else:
     image=Image.open(file)
     st.image(image,use_column_width=True)
     prediction=import_and_predict(image,model)
-    class_names=['PARASITIZED','UNINFECTED']
+    class_names=['parasitized','uninfected']
     string="OUTPUT : "+class_names[np.argmax(prediction)]
     st.success(string)
